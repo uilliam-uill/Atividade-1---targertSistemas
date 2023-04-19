729dc5db7e72d801510e9755691350682cca4596
Uilliam Santos de Aquino <126193812+uilliam-uill@users.noreply.github.com>
2023-04-19 10:28:30 -0300

    Add files via upload

---
+++ atividade1.java
@@ -0,0 +1,15 @@
+package targetSistemas;
+
+public class atividade1 {
+
+	public static void main(String[] args) {
+		int indice = 13;
+		int soma = 0;
+		
+		for(int k=0;k<indice;k+=1) {
+			soma += k;
+		}
+		System.out.println("Resultado é igual a: " + soma); //Esse é o retorno do sistema
+	}
+
+}
